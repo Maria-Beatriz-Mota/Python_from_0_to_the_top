2a1b46e8bf9225d3345d241cf9246e00254ecac5
Maria Beatriz Mota <mbeatriz.mbia@gmail.com>
2021-09-02 14:22:04 -0300

    teste_lista_Python
    primeira programação em python

---
+++ primeira programação python.py
@@ -0,0 +1,20 @@
+print('Hello People!')
+     
+
+lista = 1,2,3
+
+listan = 'a, b, c'
+
+lista3 = ['a','b',1,2]
+
+lista_aluno = []
+
+
+lista_bulit_in = list() # equivle a lista = []
+variavel_para_lista = 12
+lista_built_in_cheia = list(['a',1,variavel_para_lista])
+
+lista_a = ['1,2,3']
+lista_b = lista_a
+lista_b.append(listan)
+print("Se a =",lista_a," a  do conjuto b e n, e sabendo que n =",listan," o conjunto b será: ",lista) 
